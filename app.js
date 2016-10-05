6f4c9b7df51811dfa9c64a35675200f045eafeb0
muriel <murielthoms@gmail.com>
2016-10-05 10:57:01 +0200

    exo jq-colorpairing terminé

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-(function (){
+/*(function (){
 
 	console.log("salut");
 
 
 	function color() {
 		var buttons = $(".color");
-		buttons.css("background",($ (this).data("color"));
+		buttons.css("background",$(this).data("color"));
+
+	});
+};
+*/
+$('input').css('background',function(){
+	return $(this).data('color');
+});
+
+$('input').on("click", function() {
+	var backColor = $(this).data('color');
+	
+	if($("#modify-texte").is(':checked')){
+		$('.main').css('color', backColor);
 	}
-})()
+	else{
+		$('body').css('background', backColor);
+
+	}
+
+
+});
+
+
+
+
+
 
